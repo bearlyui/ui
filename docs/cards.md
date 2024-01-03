cd4331f9364d65947f05c8987113b810ca9cde89
Austen Cameron <austen.cameron@gmail.com>
2024-01-03 16:27:45 -0700

    wip

--- docs/cards.md
+++ docs/cards.md
@@ -4,6 +4,7 @@
 - [x] Add `header` and `footer` slots
 - [x] Add `color` property and support for all colors
 - [ ] Add `variant` property and several variants
+- [ ] Test all variant/color combos with `header` and `footer` slots
 - [ ] Figure out a better abstraction for padding/sizing
 - [ ] Finish filling out documentation
 
@@ -20,42 +21,6 @@ Use `<x-ui::card>` to create a card with the default variant, padding, and radiu
 <x-ui::card color="success">I'm a Happy Little Card</x-ui::card>
 <x-ui::card color="warning">I'm a Happy Little Card</x-ui::card>
 <x-ui::card color="error">I'm a Happy Little Card</x-ui::card>
-
-<x-ui::card>
-    <x-slot:header>Card with Header &amp; Footer</x-slot:header>
-    <div class="my-4 text-base text-black/60 dark:text-white/60">This is a card with a header. It lives in our world.</div>
-    <x-slot:footer>Example Footer</x-slot:footer>
-</x-ui::card>
-
-<x-ui::card color="primary">
-    <x-slot:header>Card with Header &amp; Footer</x-slot:header>
-    <div class="my-4 text-base text-black/60 dark:text-white/60">This is a card with a header. It lives in our world.</div>
-    <x-slot:footer>Example Footer</x-slot:footer>
-</x-ui::card>
-
-<x-ui::card color="secondary">
-    <x-slot:header>Card with Header &amp; Footer</x-slot:header>
-    <div class="my-4 text-base text-black/60 dark:text-white/60">This is a card with a header. It lives in our world.</div>
-    <x-slot:footer>Example Footer</x-slot:footer>
-</x-ui::card>
-
-<x-ui::card color="success">
-    <x-slot:header>Card with Header &amp; Footer</x-slot:header>
-    <div class="my-4 text-base text-black/60 dark:text-white/60">This is a card with a header. It lives in our world.</div>
-    <x-slot:footer>Example Footer</x-slot:footer>
-</x-ui::card>
-
-<x-ui::card color="warning">
-    <x-slot:header>Card with Header &amp; Footer</x-slot:header>
-    <div class="my-4 text-base text-black/60 dark:text-white/60">This is a card with a header. It lives in our world.</div>
-    <x-slot:footer>Example Footer</x-slot:footer>
-</x-ui::card>
-
-<x-ui::card color="error">
-    <x-slot:header>Card with Header &amp; Footer</x-slot:header>
-    <div class="my-4 text-base text-black/60 dark:text-white/60">This is a card with a header. It lives in our world.</div>
-    <x-slot:footer>Example Footer</x-slot:footer>
-</x-ui::card>
 ```
 
 ## Properties
@@ -78,9 +43,46 @@ TODO
 
 TODO
 
-### ☑️ Variant
+### Variant
 
-TODO
+Cards come in several variants: `solid`, `outline`, `glow`, and `gradient`. The default is `outline`.
+
+```html +demo title={Outline Variant} previewClasses={space-y-3}
+<x-ui::card>I'm a happy little card.</x-ui::card>
+<x-ui::card color="primary">I'm a happy little primary card.</x-ui::card>
+<x-ui::card color="secondary">I'm a happy little secondary card.</x-ui::card>
+<x-ui::card color="success">I'm a happy little success card.</x-ui::card>
+<x-ui::card color="warning">I'm a happy little warning card.</x-ui::card>
+<x-ui::card color="error">I'm a happy little error card.</x-ui::card>
+```
+
+```html +demo title={Solid Variant} previewClasses={space-y-3}
+<x-ui::card variant="solid">I'm a happy little card.</x-ui::card>
+<x-ui::card variant="solid" color="primary">I'm a happy little primary card.</x-ui::card>
+<x-ui::card variant="solid" color="secondary">I'm a happy little secondary card.</x-ui::card>
+<x-ui::card variant="solid" color="success">I'm a happy little success card.</x-ui::card>
+<x-ui::card variant="solid" color="warning">I'm a happy little warning card.</x-ui::card>
+<x-ui::card variant="solid" color="error">I'm a happy little error card.</x-ui::card>
+```
+
+```html +demo title={Gradient Variant} previewClasses={space-y-3}
+<x-ui::card variant="gradient">I'm a happy little card.</x-ui::card>
+<x-ui::card variant="gradient" color="primary">I'm a happy little primary card.</x-ui::card>
+<x-ui::card variant="gradient" color="secondary">I'm a happy little secondary card.</x-ui::card>
+<x-ui::card variant="gradient" color="success">I'm a happy little success card.</x-ui::card>
+<x-ui::card variant="gradient" color="warning">I'm a happy little warning card.</x-ui::card>
+<x-ui::card variant="gradient" color="error">I'm a happy little error card.</x-ui::card>
+```
+
+```html +demo title={Glow Variant} previewClasses={space-y-3}
+<x-ui::card variant="glow">I'm a happy little card.</x-ui::card>
+<x-ui::card variant="glow" color="primary">I'm a happy little primary card.</x-ui::card>
+<x-ui::card variant="glow" color="secondary">I'm a happy little secondary card.</x-ui::card>
+<x-ui::card variant="glow" color="success">I'm a happy little success card.</x-ui::card>
+<x-ui::card variant="glow" color="warning">I'm a happy little warning card.</x-ui::card>
+<x-ui::card variant="glow" color="error">I'm a happy little error card.</x-ui::card>
+```
+
 
 ## Slots
 
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -6,17 +6,44 @@
     'variant' => Variant::Outline,
 ])
 <div {{ $attributes->class([
-    'rounded border shadow dark:shadow-md bg-white dark:bg-gray-400/5 ',
-    'bg-white dark:bg-gray-400/5',
+    'rounded border',
     'text-gray-700 dark:text-gray-200',
 
-    {{-- Colors --}}
-    'border-gray-300/75 dark:border-white/7.5' => Color::None->is($color) && Variant::Outline->is($variant),
-    'border-primary-300/50' => Color::Primary->is($color) && Variant::Outline->is($variant),
-    'border-secondary-300/50' => Color::Secondary->is($color) && Variant::Outline->is($variant),
-    'border-success-300/50' => Color::Success->is($color) && Variant::Outline->is($variant),
-    'border-warning-300/50' => Color::Warning->is($color) && Variant::Outline->is($variant),
-    'border-error-300/50' => Color::Error->is($color) && Variant::Outline->is($variant),
+    'shadow dark:shadow-md' => !Variant::Glow->is($variant),
+    'bg-white dark:bg-gray-400/5' => Variant::Outline->is($variant) || Variant::Glow->is($variant),
+
+    {{-- Outline --}}
+    'border-gray-300 dark:border-white/7.5' => Color::None->is($color) && Variant::Outline->is($variant),
+    'border-primary-300 dark:border-primary-300/50' => Color::Primary->is($color) && Variant::Outline->is($variant),
+    'border-secondary-300 dark:border-secondary-300/50' => Color::Secondary->is($color) && Variant::Outline->is($variant),
+    'border-success-300 dark:border-success-300/50' => Color::Success->is($color) && Variant::Outline->is($variant),
+    'border-warning-300 dark:border-warning-300/50' => Color::Warning->is($color) && Variant::Outline->is($variant),
+    'border-error-300 dark:border-error-300/50' => Color::Error->is($color) && Variant::Outline->is($variant),
+
+    {{-- Solid --}}
+    'bg-white dark:bg-gray-400/5 border-gray-300/75 dark:border-white/7.5' => Color::None->is($color) && Variant::Solid->is($variant),
+    'bg-primary-100 border-primary-400 text-primary-800 dark:bg-primary-400/40 dark:text-primary-200' => Color::Primary->is($color) && Variant::Solid->is($variant),
+    'bg-secondary-100 border-secondary-300 text-secondary-800 dark:bg-secondary-400/40 dark:text-secondary-200' => Color::Secondary->is($color) && Variant::Solid->is($variant),
+    'bg-success-100 border-success-400 text-success-800 dark:bg-success-400/40 dark:text-success-200' => Color::Success->is($color) && Variant::Solid->is($variant),
+    'bg-warning-100 border-warning-400 text-warning-800 dark:bg-warning-400/40 dark:text-warning-100' => Color::Warning->is($color) && Variant::Solid->is($variant),
+    'bg-error-100 border-error-400/75 text-error-800 dark:bg-error-400/40 dark:text-error-200' => Color::Error->is($color) && Variant::Solid->is($variant),
+
+    {{-- Gradient --}}
+    'bg-gradient-to-br from-white to-gray-50 dark:from-gray-400/5 dark:to-gray-400/10 border-gray-300/75 dark:border-white/7.5' => Color::None->is($color) && Variant::Gradient->is($variant),
+    'bg-gradient-to-br from-primary-50 to-primary-200 dark:from-primary-300/10 dark:to-primary-900/80 dark:border-primary-400/40 dark:text-primary-200' => Color::Primary->is($color) && Variant::Gradient->is($variant),
+    'bg-gradient-to-br from-secondary-50 to-secondary-200 dark:from-secondary-300/10 dark:to-secondary-900/80 dark:border-secondary-400/40 dark:text-secondary-200' => Color::Secondary->is($color) && Variant::Gradient->is($variant),
+    'bg-gradient-to-br from-success-50 to-success-200 dark:from-success-300/10 dark:to-success-900/80 dark:border-success-400/40 dark:text-success-200' => Color::Success->is($color) && Variant::Gradient->is($variant),
+    'bg-gradient-to-br from-warning-50 to-warning-200 dark:from-warning-300/10 dark:to-warning-900/80 dark:border-warning-400/40 dark:text-warning-200' => Color::Warning->is($color) && Variant::Gradient->is($variant),
+    'bg-gradient-to-br from-error-50 to-error-200 dark:from-error-300/10 dark:to-error-900/80 dark:border-error-400/40 dark:text-error-200' => Color::Error->is($color) && Variant::Gradient->is($variant),
+
+    {{-- Glow --}}
+    'shadow-lg bg-white dark:bg-gray-400/5' => Variant::Glow->is($variant),
+    'border-gray-300 dark:border-white/7.5' => Color::None->is($color) && Variant::Glow->is($variant),
+    'border-primary-500/40 shadow-primary-400/30' => Color::Primary->is($color) && Variant::Glow->is($variant),
+    'border-secondary-500/40 shadow-secondary-400/30' => Color::Secondary->is($color) && Variant::Glow->is($variant),
+    'border-success-500/40 shadow-success-400/30' => Color::Success->is($color) && Variant::Glow->is($variant),
+    'border-warning-500/40 shadow-warning-400/30' => Color::Warning->is($color) && Variant::Glow->is($variant),
+    'border-error-500/40 shadow-error-400/30' => Color::Error->is($color) && Variant::Glow->is($variant),
 ]) }}>
 
     {{-- Header --}}
