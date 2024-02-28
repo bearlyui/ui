64356a1184bfdc2885154290d7d8c706a4b20ef0
Austen Cameron <austen.cameron@gmail.com>
2024-02-27 22:51:43 -0700

    wip

--- src/BearUiProvider.php
+++ src/BearUiProvider.php
@@ -8,7 +8,7 @@ class BearUiProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->commands([Commands\Install::class]);
+        $this->commands([Commands\Add::class, Commands\Install::class]);
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'ui');
         $this->mergeConfigFrom(__DIR__.'/../config/ui.php', 'ui');
     }
--- src/Commands/Add.php
+++ src/Commands/Add.php
@@ -3,151 +3,152 @@
 namespace Bearly\Ui\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Process;
 
 use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\info;
+use function Laravel\Prompts\multiselect;
 use function Laravel\Prompts\note;
+use function Laravel\Prompts\table;
 use function Laravel\Prompts\text;
+use function Laravel\Prompts\warning;
 
-class Install extends Command
+class Add extends Command
 {
-    public $signature = 'bear:install';
+    public $signature = 'bear:add';
 
-    // TODO: Ensure tailwind is actually installed in app.css (with confirmation)
-    // TODO: Ensure tailwind is installed with autoprefixer and stuff
-    // TODO: Add / initialize an app layout for livewire if one doesn't exist (with confirmation)
-    // TODO: Add a test route at / URL to ensure everything is working -- it should go to a demo view that shows some buttons being used
-    // TODO: Test that we can do the following: `composer require bearly/ui && php artisan bear-ui:install` and it works with a demo page
-    // TODO: Add prefix setting/class/prompt to affect publish path
-
-    protected function ensureJsFileHasValues(string $path, string $key, array $values)
-    {
-        $jsFile = File::get(base_path($path));
-        $arrayFromFile = str($jsFile)->match("/{$key}:[\s]*?\[.*?\],?/sm")
-            ->after('[')
-            ->beforeLast(']')
-            ->replaceMatches('/\s/', '')
-            ->trim()
-            ->explode(',')
-            ->filter()
-            ->push($values)
-            ->flatten()
-            ->unique();
-
-        File::put(
-            path: base_path($path),
-            contents: str($jsFile)->replaceMatches(
-                "/{$key}:[\s]*?\[.*?\],?/sm",
-                str("{$key}: [\n    ")->append($arrayFromFile->implode(",\n    "))->append("\n  ],\n")
-
-            )
-        );
-    }
-
-    protected function installLivewire()
-    {
-        note('🛠️  Checking for Livewire installation...');
-
-        $livewireInstalled = str(File::get(base_path('composer.json')))->contains('livewire/livewire');
-
-        if ($livewireInstalled) {
-            info('👍  Livewire is already installed.');
-
-            return;
-        }
-
-        Process::run('composer require livewire/livewire', function ($type, $output) {
-            // echo $output;
-        })->throw();
-        info('✅  Livewire installed.');
-    }
-
-    protected function installTailwind()
-    {
-        note('🛠️  Checking Tailwind CSS installation...');
-
-        $this->tailwindPackagesInstalled();
-        $this->tailwindInAppCss();
-        $this->installBearUiTailwindPlugin();
-        $this->ensureJsFileHasValues('tailwind.config.js', 'content', ["'./resources/**/*.blade.php'", "'./app/**/*.php'"]);
-    }
-
-    protected function tailwindPackagesInstalled()
-    {
-        $tailwindAndRequirementsInstalled = str(File::get(base_path('package.json')))
-            ->containsAll([
-                '"tailwindcss":',
-                '"@tailwindcss/forms":',
-                '"postcss":',
-                '"autoprefixer":',
-            ]);
-
-        if (! $tailwindAndRequirementsInstalled) {
-            if (confirm('⛔️  Tailwind CSS and its required packages are not installed. Do you want to install them now?')) {
-                Process::run('npm install -D tailwindcss postcss autoprefixer @tailwindcss/forms', function ($type, $output) {
-                    // echo $output;
-                })->throw();
-                info('✅  Installed Tailwind CSS and required packages.');
-
-                Process::run('npx tailwindcss init -p', fn ($type, $output) => null/* note($output)*/);
-            }
-        }
-    }
-
-    protected function tailwindInAppCss()
-    {
-        $appCssFile = File::get(base_path('resources/css/app.css'));
-        if (! str($appCssFile)->contains([
-            '@tailwind base',
-            '@tailwind components',
-            '@tailwind utilities',
-        ])) {
-            File::put(
-                path: base_path('resources/css/app.css'),
-                contents: str($appCssFile)->prepend(
-                    "\n@tailwind base;\n",
-                    "@tailwind components;\n",
-                    "@tailwind utilities;\n",
-                )
-            );
-        }
-    }
-
-    protected function installBearUiTailwindPlugin()
-    {
-        if (! File::exists(base_path('tailwind.config.js'))) {
-            if (confirm('⛔️  No tailwind.config.js file found. Do you want to create one now?')) {
-
-                // First, publish Tailwind and PostCSS files from tailwind
-                Process::run('npx tailwindcss init -p', function (string $type, string $output) {
-                    // echo $output;
-                })->throw();
-            }
-        }
-
-        // Get the tailwind config file and check if it has the forms plugin
-        $tailwindConfig = str(File::get(base_path('tailwind.config.js')));
-
-        // Do we have the import statement?
-        if (! $tailwindConfig->contains("import bearUI from './vendor/bearly/ui/ui'")) {
-            $tailwindConfig = $tailwindConfig->prepend("import bearUI from './vendor/bearly/ui/ui'\n");
-            File::put(base_path('tailwind.config.js'), $tailwindConfig);
-        }
-        $this->ensureJsFileHasValues('tailwind.config.js', 'plugins', ['bearUI']);
-
-        info('✅  Bear UI Tailwind CSS plugin installed.');
-    }
+    public $allComponents = [
+        'alert' => 'Alert',
+        'card' => 'Card',
+        'button' => 'Button',
+        'form-inputs' => 'Form Inputs',
+        'dropdown' => 'Dropdown',
+        'tooltip' => 'Tooltip',
+    ];
 
     public function handle()
     {
+        // Welcome art and greeting
         $this->welcome();
-        $this->installTailwind();
-        $this->newLine();
-        $this->installLivewire();
-        $this->call('bear:add');
+
+        // Publish location
+        note('🗡️  Where should the blade components be published?');
+        $publishTo = text(
+            label: 'Blade Component Publish Path',
+            default: 'resources/views/components',
+            hint: 'Relative to the base path of your Laravel app.'
+        );
+
+        // Choose components
+        note('📦  Choose components to publish.');
+        $componentsToPublish = multiselect(
+            label: 'Choose components or press enter publish all',
+            options: $this->allComponents,
+            default: ['alert', 'card', 'button', 'form-inputs', 'dropdown', 'tooltip'],
+            scroll: 10,
+            hint: 'Spacebar to select, Enter to confirm.'
+        );
+
+        // Publish the components
+        if (confirm(
+            label: sprintf('Publish selected components to %s?', $publishTo),
+            default: true,
+        )) {
+            $this->publish($componentsToPublish, $publishTo);
+        }
+    }
+
+    protected function publish(array $components, string $publishTo)
+    {
+        info('🫡  Publishing...');
+
+        foreach ($this->allComponents as $slug => $title) {
+
+            // Skip components that weren't selected to be published
+            if (! in_array($slug, $components)) {
+                continue;
+            }
+
+            $publishToPath = base_path($publishTo.DIRECTORY_SEPARATOR);
+
+            // Some files have multiple paths
+            foreach ($this->pathsFromSlug($slug) as $path) {
+
+                // Create the folder if it doesn't exist
+                if (! File::isDirectory(str($publishToPath.$path)->beforeLast(DIRECTORY_SEPARATOR))) {
+                    File::makeDirectory(
+                        path: str($publishToPath.$path)->beforeLast(DIRECTORY_SEPARATOR),
+                        recursive: true
+                    );
+                }
+
+                if (file_exists($publishToPath.$path.'.blade.php')) {
+                    if (! confirm(
+                        sprintf('%s%s%s.blade.php already exists. Overwrite?', $publishTo, DIRECTORY_SEPARATOR, $path),
+                        default: false
+                    )) {
+                        warning(sprintf('⚠️  Skipping %s...', str($path)->headline()));
+
+                        continue;
+                    }
+                }
+
+                // Copy the file from package's folder to the publish location
+                note(sprintf('Publishing %s%s%s.blade.php...', $publishTo, DIRECTORY_SEPARATOR, $path));
+                copy(
+                    __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.$path.'.blade.php',
+                    $publishToPath.$path.'.blade.php'
+                );
+
+            }
+        }
+
+        info("🎉  Done! \n 📝  You can now use the components in your views.");
+        table(
+            ['Component', 'Result', 'Published To'],
+            collect($this->allComponents)->map(fn ($title, $slug) => [
+                $title,
+                in_array($slug, $components) ? '✅' : '❌',
+                in_array($slug, $components) ? $publishTo.DIRECTORY_SEPARATOR.$slug.'.blade.php' : '',
+            ])
+        );
+
+        info('♻️ Run `npm run build` to compile assets?');
+        $runNpmBuild = confirm(
+            label: 'Run npm run build?',
+            default: true,
+            hint: 'This will compile the assets.'
+        );
+        // Run npm build
+        if ($runNpmBuild) {
+            info('🔨 Running npm run build...');
+            echo `npm run build`;
+        }
+
+    }
+
+    protected function pathsFromSlug($slug): array
+    {
+        return match ($slug) {
+            'dropdown' => [
+                'dropdown'.DIRECTORY_SEPARATOR.'index',
+                'dropdown'.DIRECTORY_SEPARATOR.'item',
+            ],
+            'form-inputs' => [
+                'checkbox',
+                'fieldset',
+                'input-error',
+                'input-group',
+                'input',
+                'label',
+                'radio',
+                'select',
+                'textarea',
+            ],
+            'tooltip' => ['tooltip', 'kbd'],
+            'toggle-switch' => ['toggle'],
+            default => [$slug],
+        };
     }
 
     protected function welcome()
