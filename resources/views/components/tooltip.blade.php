6bd726a64ce30f6082e23051242125c6d46022ec
Austen Cameron <austen.cameron@gmail.com>
2024-10-10 16:47:16 -0600

    wip

--- resources/views/components/tooltip.blade.php
+++ resources/views/components/tooltip.blade.php
@@ -17,7 +17,6 @@
     <template x-teleport="body">
         <span
             x-cloak
-            class="absolute w-max pointer-events-none z-50"
             x-anchor.{{ $position }}.offset.{{ $offset }}="trigger"
             x-data="{
                 show: false,
@@ -35,22 +34,23 @@
                     trigger.removeEventListener('mouseleave', this.closeHandler)
                 }
             }"
+            class="absolute w-max pointer-events-none z-50"
         >
 
             <span
                 x-show="show"
                 x-transition
                 role="tooltip"
-                @class([
+                {{ $attributes->class([
                     'block w-max transition-all ease-in-out pointer-events-none',
                     'border rounded backdrop-blur-lg',
-                    'bg-gradient-to-b from-white/90 to-white/60 border-black/15a text-gray-700 shadow',
+                    'bg-gradient-to-b from-white/80 to-white/50 border-black/15a text-gray-700 shadow',
                     'dark:from-white/10 dark:to-white/5 dark:border-white/10 dark:text-white/80 dark:shadow-lg dark:shadow-black/30',
                     'px-1.5 py-1 text-xs' => Size::SM->is($size),
                     'px-3 py-1.5 text-sm' => Size::BASE->is($size),
                     'px-4 py-2 text-base' => Size::MD->is($size),
                     'px-5 py-2.5 text-lg' => Size::LG->is($size),
-                ])
+                ]) }}
             >
                 @if ($title)
                     <span x-text="title"></span>
