33e08e7033982edb98365923dfd6f64257dd81e6
Austen Cameron <austen.cameron@gmail.com>
2024-10-17 00:00:59 -0600

    wip

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -5,10 +5,10 @@
     'size' => Size::BASE,
 ])
 
-<{{ $tag }} {{ $attributes->class([
+<{{ $tag }} data-ui-heading {{ $attributes->class([
     'ui-heading', // Needed for the description component to add margin top
     'text-gray-700 dark:text-gray-300',
-    'text-lg tracking-[-0.01em] font-medium' => Size::BASE->is($size),
+    'text-lg tracking-[-0.015em] font-medium' => Size::BASE->is($size),
     'text-xl tracking-tight font-medium' => Size::MD->is($size),
     'text-2xl tracking-tight font-semibold' => Size::LG->is($size),
     'text-2xl tracking-tighter font-extrabold' => Size::XL->is($size),
