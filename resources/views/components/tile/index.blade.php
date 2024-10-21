735f63c5ecf7c134e530610bcf4759b485b0f69e
Austen Cameron <austen.cameron@gmail.com>
2024-10-21 11:07:22 -0600

    add link, change random stuff

--- resources/views/components/tile/index.blade.php
+++ resources/views/components/tile/index.blade.php
@@ -2,9 +2,11 @@
     'flex-1 flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8'
 ])>
     @if (!empty($label))
-        <dt {{ $label->attributes->class(['text-sm font-medium leading-6 text-gray-500']) }}>
+        <ui:description tag="dt" :attributes="$label->attributes->class([
+            'text-sm font-medium leading-6 opacity-40',
+        ])">
             {{ $label }}
-        </dt>
+        </ui:description>
     @endif
 
     @if (!empty($description))
@@ -13,7 +15,9 @@
         </dd>
     @endif
 
-    <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">
+    <ui:heading tag="dd" size="lg" :attributes="$slot->attributes->class([
+        'w-full flex-none leading-10',
+    ])">
         {{ $slot }}
-    </dd>
+    </ui:heading>
 </div>
