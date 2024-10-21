d243b8734c417e225caf2729f62718d5a4156218
Austen Cameron <austen.cameron@gmail.com>
2024-10-21 11:28:41 -0600

    wip

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -3,7 +3,9 @@
     'href' => null,
 ])
 
-<a {{ $attributes->when(
-    $when === true && $href !== null,
-    fn ($attributes) => $attributes->merge(['href' => $href])
-) }}>{{ $slot }}</a>
+<a {{ $attributes
+    ->when(
+        $when === true && $href !== null,
+        fn ($attributes) => $attributes->merge(['href' => $href])
+    )
+}}>{{ $slot }}</a>
