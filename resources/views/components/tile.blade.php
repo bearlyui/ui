56cda456effb651d1816a0d2510abfc24aa1cb6a
Austen Cameron <austen.cameron@gmail.com>
2024-10-21 11:58:52 -0600

    update tiles

--- resources/views/components/tile.blade.php
+++ resources/views/components/tile.blade.php
@@ -1,5 +1,10 @@
+@use('Bearly\Ui\Variant')
+@aware(['variant' => Variant::Solid])
 <div @class([
-    'flex-1 flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8'
+    'flex-1 flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 px-4 py-10 sm:px-6 xl:px-8',
+    'bg-white dark:bg-gray-900/50' => Variant::Solid->is($variant),
+    'bg-white border border-gray-900/10 dark:border-white/5' => Variant::Outline->is($variant),
+    'bg-transparent' => Variant::Ghost->is($variant),
 ])>
     @if (!empty($label))
         <ui:description tag="dt" :attributes="$label->attributes->class([
--- resources/views/components/tiles.blade.php
+++ resources/views/components/tiles.blade.php
@@ -1,8 +1,15 @@
 @use('Bearly\Ui\Size')
-@props(['gap' => Size::BASE])
+@use('Bearly\Ui\Variant')
+
+@props([
+    'gap' => Size::BASE,
+    'variant' => Variant::Outline,
+])
 
 <dl {{ $attributes->class([
     'mx-auto flex flex-col bg-gray-900/5 sm:flex-row',
+    'bg-gray-900/5' => Variant::Solid->is($variant),
+    'bg-transparent' => Variant::Outline->is($variant) || Variant::Ghost->is($variant),
     'gap-px' => Size::BASE->is($gap),
     'gap-0.5' => Size::MD->is($gap),
     'gap-1' => Size::LG->is($gap),
