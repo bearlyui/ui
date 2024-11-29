87ee43b0c013a4468c82863674b15302ca737873
Austen Cameron <austen.cameron@gmail.com>
2024-11-29 15:30:02 -0700

    wip

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -47,13 +47,13 @@
             'px-8 py-4 text-xl font-medium' => Size::XL->is($size),
 
             {{-- Base Variant Styles --}}
-            'border shadow-sm' => Variant::Outline->is($variant) || Variant::Solid->is($variant),
+            'border shadow-sm hover:shadow' => Variant::Outline->is($variant) || Variant::Solid->is($variant),
             {{-- '[text-shadow:1px_0.5px_0px_rgba(0,0,0,0.24)]' => Variant::Solid->is($variant) || Variant::Gradient->is($variant), --}}
 
             {{-- Solid Variant --}}
-            'bg-primary-400 text-primary-900 border-primary-600/60 hover:bg-primary-700 hover:text-white dark:text-primary-300 dark:hover:text-primary-200 dark:border-primary-500/30 dark:bg-primary-400/30 dark:hover:bg-primary-400/50' => Color::Primary->is($color) && Variant::Solid->is($variant),
+            'bg-primary-700 text-white border-primary-600/60 hover:bg-primary-900 dark:text-primary-300 dark:hover:text-primary-200 dark:border-primary-500/30 dark:bg-primary-400/30 dark:hover:bg-primary-400/50' => Color::Primary->is($color) && Variant::Solid->is($variant),
             '[text-shadow:none] bg-secondary-50 border-secondary-300/60 text-secondary-600/90 hover:bg-white hover:border-secondary-300/80 hover:text-secondary-700 dark:bg-secondary-300/10 dark:border-secondary-600 dark:text-secondary-300 dark:hover:bg-primary-300/15 dark:hover:text-secondary-100 dark:hover:border-primary-300/30' => Color::Secondary->is($color) && Variant::Solid->is($variant),
-            'bg-success-400 text-success-900 border-success-600/60 hover:bg-success-600 hover:text-white dark:text-success-500 dark:hover:text-success-200 dark:border-success-500/30 dark:bg-success-400/15 dark:hover:bg-success-400/40' => Color::Success->is($color) && Variant::Solid->is($variant),
+            'bg-success-500 text-success-950/90 border-success-600/60 hover:bg-success-600 hover:text-white dark:text-success-500 dark:hover:text-success-200 dark:border-success-500/30 dark:bg-success-400/15 dark:hover:bg-success-400/40' => Color::Success->is($color) && Variant::Solid->is($variant),
             'bg-warning-400 text-warning-900 border-warning-600/60 hover:bg-warning-600 hover:text-white dark:text-warning-500 dark:hover:text-warning-200 dark:border-warning-500/30 dark:bg-warning-400/15 dark:hover:bg-warning-400/40' => Color::Warning->is($color) && Variant::Solid->is($variant),
             'bg-error-500/80 text-error-950/90 border-error-600/60 hover:bg-error-700 hover:text-white dark:text-error-400 dark:hover:text-error-200 dark:border-error-500/30 dark:bg-error-500/15 dark:hover:bg-error-400/40' => Color::Error->is($color) && Variant::Solid->is($variant),
 
