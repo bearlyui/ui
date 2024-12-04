1b02ccef6a24f2c93b482de7cefc958525466212
Austen Cameron <austen.cameron@gmail.com>
2024-12-04 10:43:53 -0700

    wip

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -6,6 +6,6 @@
 <a {{ $attributes
     ->when(
         $when === true && $href !== null,
-        fn ($attributes) => $attributes->merge(['href' => $href])
+        fn ($attributes) => $attributes->merge(['href' => $href])->class('text-primary-600 hover:text-primary-800 dark:text-primary-400 dark:hover:text-primary-200')
     )
 }}>{{ $slot }}</a>
