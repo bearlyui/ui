14e31968d91aeffe7968ad4d81960876533bda85
Austen Cameron <austen.cameron@gmail.com>
2024-12-09 14:11:32 -0700

    extract alpine from dialog

--- resources/views/components/dialog.blade.php
+++ resources/views/components/dialog.blade.php
@@ -6,42 +6,7 @@
 ])
 
 <span
-    x-data="{
-        open: false,
-        removedAriaHidden: false,
-        init() {
-            this.$nextTick(() => {
-                const heading = this.$refs.content.querySelector('[data-ui-heading]')
-                if (heading) {
-                    heading.setAttribute('x-bind:id', '$id(\'dialog-title\')')
-                    this.$refs.content.setAttribute('x-bind:aria-labelledby', '$id(\'dialog-title\')')
-                }
-
-                const subheading = this.$refs.content.querySelector('[data-ui-subheading]')
-                if (subheading) {
-                    subheading.setAttribute('x-bind:id', '$id(\'dialog-description\')')
-                    this.$refs.content.setAttribute('x-bind:aria-describedby', '$id(\'dialog-description\')')
-                }
-            })
-        },
-        openDialog() {
-            this.open = true
-
-            if ($refs.dialog.hasAttribute('aria-hidden')) {
-                $refs.dialog.removeAttribute('aria-hidden')
-                this.removedAriaHidden = true
-            }
-        },
-        closeDialog() {
-            this.open = false
-            this.removedAriaHidden && $refs.dialog.setAttribute('aria-hidden', 'true')
-            this.$refs.close?.setAttribute('inert', 'true')
-        },
-        closeOnEscape() {
-            $refs.dialog.getAttribute('aria-hidden') === null && this.closeDialog()
-        }
-    }"
-    x-modelable="open"
+    x-data="uiDialog"
     {{ $attributes }}
 >
     {{-- Trigger --}}
@@ -50,10 +15,7 @@
     {{-- Dialog --}}
     <template x-teleport="body">
         <div
-            x-show="open"
-            x-trap.inert.noscroll="open"
-            x-on:keyup.escape.window="closeOnEscape"
-            x-ref="dialog"
+            x-bind="container"
             @class([
                 'sm:px-5' => $size === 'full',
                 'fixed flex items-end sm:items-center justify-center top-0 right-0 bottom-0 left-0',
@@ -62,24 +24,13 @@
         >
             {{-- Overlay --}}
             <div
+                x-bind="overlay"
                 class="fixed sm:p-4 md:px-0 top-0 bottom-0 left-0 right-0 w-full h-full bg-white/30 dark:bg-black/30 backdrop-blur"
-                x-show="open"
-                x-transition:enter.opacity.delay.0ms
-                x-transition:leave.delay.0ms.duration.0ms
-                @click.stop="closeDialog"
             ></div>
 
             {{-- Content --}}
             <div
-                x-ref="content"
-                x-show="open"
-                x-transition:enter="transition ease-out duration-150 delay-75 origin-bottom"
-                x-transition:enter-start="opacity-0 transform scale-90 translate-y-4"
-                x-transition:enter-end="opacity-100 transform scale-100 translate-y-0"
-                x-transition:leave.duration.0ms.delay.0ms
-                role="dialog"
-                aria-modal="true"
-                x-id="['dialog-title', 'dialog-description']"
+                x-bind="content"
                 @class([
                     'rounded flex-1 shadow-lg relative not-prose mx-auto max-w-full w-full max-h-[96vh] overflow-y-auto ring-1 ring-black/5 dark:ring-gray-700/60',
                     'sm:max-w-xl' => $size === 'sm',
@@ -123,10 +74,7 @@
                                 size="sm"
                                 variant="ghost"
                                 class="group"
-                                inert
-                                x-ref="close"
-                                x-on:click="closeDialog"
-                                x-effect="open && setTimeout(() => $el.removeAttribute('inert'), 100)"
+                                x-bind="uiDialogClose"
                             >
                                 <span class="sr-only">Close</span>
                                 <ui:icon.x-mark class="opacity-75 group-hover:opacity-100" variant="mini" />
@@ -143,10 +91,7 @@
                                     size="sm"
                                     variant="ghost"
                                     class="group"
-                                    inert
-                                    x-ref="close"
-                                    x-on:click="closeDialog"
-                                    x-effect="open && setTimeout(() => $el.removeAttribute('inert'), 100)"
+                                    x-bind="uiDialogClose"
                                     @class([
                                         'absolute top-0 right-0 mr-1 mt-1',
                                         'hidden' => $hideCloseButton,
