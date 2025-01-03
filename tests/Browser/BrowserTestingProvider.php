91236670b15c55fc42d53a8603a415018acd48a7
Austen Cameron <austen.cameron@gmail.com>
2025-01-02 21:05:43 -0700

    wip

--- tests/Browser/BrowserTestingProvider.php
+++ tests/Browser/BrowserTestingProvider.php
@@ -10,11 +10,6 @@ class BrowserTestingProvider extends ServiceProvider
 {
     public function boot()
     {
-        \Livewire\Livewire::component('example-livewire-dialog', ExampleLivewireDialog::class);
-        \Livewire\Livewire::component('example-livewire-toggle', ExampleLivewireToggle::class);
-        \Livewire\Livewire::component('example-livewire-toggle-array', ExampleLivewireToggleArray::class);
-        \Livewire\Livewire::component('example-livewire-toggle-single-error', ExampleLivewireToggleSingleError::class);
-
         Browser::macro('pressEscape', fn () => $this->withKeyboard(fn ($k) => $k->sendKeys(WebDriverKeys::ESCAPE)));
         Browser::macro('hoverWithJs', function ($selector) {
             $this->script("document.querySelector('{$selector}').dispatchEvent(new MouseEvent('mouseover', { bubbles: true }))");
