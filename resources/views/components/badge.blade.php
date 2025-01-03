182b6d40302b58fd44e3abae04cba86fa0cc8d6a
Austen Cameron <austen.cameron@gmail.com>
2025-01-03 12:58:53 -0700

    Add Badge component - AC-189 (#35)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ resources/views/components/badge.blade.php
@@ -0,0 +1,79 @@
+@use('Bearly\Ui\Size')
+@use('Bearly\Ui\Color')
+@use('Bearly\Ui\Variant')
+
+@props([
+    'size' => Size::BASE,
+    'radius' => Size::BASE,
+    'color' => Color::Secondary,
+    'variant' => Variant::Solid,
+    'icon' => null,
+    'iconAfter' => null,
+    'iconVariant' => 'micro',
+])
+<{{ $attributes->has('href') ? 'a' : 'span' }}
+{{ $attributes->class([
+    'inline-flex items-center tabular-nums transition',
+
+    {{-- Solid Variant --}}
+    'bg-primary-200/60 text-primary-900 dark:bg-primary-400/20 dark:text-primary-100' => Variant::Solid->is($variant) && Color::Primary->is($color),
+    'hover:bg-primary-300/60 hover:text-primary-900 dark:hover:bg-primary-300/25 dark:hover:text-primary-100' => $attributes->has('href') && Variant::Solid->is($variant) && Color::Primary->is($color),
+    'bg-secondary-300/40 text-secondary-800 dark:bg-secondary-400/20 dark:text-secondary-100' => Variant::Solid->is($variant) && Color::Secondary->is($color),
+    'hover:bg-secondary-300/75 hover:text-secondary-900 dark:hover:bg-secondary-300/25 dark:hover:text-secondary-100' => $attributes->has('href') && Variant::Solid->is($variant) && Color::Secondary->is($color),
+    'bg-success-200/60 text-success-900 dark:bg-success-400/20 dark:text-success-100' => Variant::Solid->is($variant) && Color::Success->is($color),
+    'hover:bg-success-300/60 hover:text-success-900 dark:hover:bg-success-300/25 dark:hover:text-success-100' => $attributes->has('href') && Variant::Solid->is($variant) && Color::Success->is($color),
+    'bg-warning-200/60 text-warning-900 dark:bg-warning-400/20 dark:text-warning-100' => Variant::Solid->is($variant) && Color::Warning->is($color),
+    'hover:bg-warning-300/60 hover:text-warning-900 dark:hover:bg-warning-300/25 dark:hover:text-warning-100' => $attributes->has('href') && Variant::Solid->is($variant) && Color::Warning->is($color),
+    'bg-danger-200/60 text-danger-900 dark:bg-danger-400/20 dark:text-danger-100' => Variant::Solid->is($variant) && Color::Danger->is($color),
+    'hover:bg-danger-300/60 hover:text-danger-900 dark:hover:bg-danger-300/25 dark:hover:text-danger-100' => $attributes->has('href') && Variant::Solid->is($variant) && Color::Danger->is($color),
+
+    {{-- Outline Variant --}}
+    'bg-transparent border border-primary-600/70 text-primary-800 dark:border-primary-400/60 dark:text-primary-300/85' => Variant::Outline->is($variant) && Color::Primary->is($color),
+    'hover:bg-primary-300/15 hover:border-primary-400/50 hover:text-primary-900 dark:hover:bg-primary-400/20 dark:hover:text-primary-100' => $attributes->has('href') && Variant::Outline->is($variant) && Color::Primary->is($color),
+    'bg-transparent border border-secondary-600/40 text-secondary-700 dark:border-secondary-400/60 dark:text-secondary-300' => Variant::Outline->is($variant) && Color::Secondary->is($color),
+    'hover:bg-secondary-300/50 hover:border-secondary-400/50 hover:text-secondary-900 dark:hover:bg-secondary-400/20 dark:hover:text-secondary-100' => $attributes->has('href') && Variant::Outline->is($variant) && Color::Secondary->is($color),
+    'bg-transparent border border-success-600/60 text-success-800 dark:border-success-400/60 dark:text-success-300/85' => Variant::Outline->is($variant) && Color::Success->is($color),
+    'hover:bg-success-300/50 hover:border-success-400/50 hover:text-success-900 dark:hover:bg-success-400/20 dark:hover:text-success-100' => $attributes->has('href') && Variant::Outline->is($variant) && Color::Success->is($color),
+    'bg-transparent border border-warning-700/60 text-warning-800 dark:border-warning-400/60 dark:text-warning-300/85' => Variant::Outline->is($variant) && Color::Warning->is($color),
+    'hover:bg-warning-300/50 hover:border-warning-400/50 hover:text-warning-900 dark:hover:bg-warning-400/20 dark:hover:text-warning-100' => $attributes->has('href') && Variant::Outline->is($variant) && Color::Warning->is($color),
+    'bg-transparent border border-danger-700/60 text-danger-700 dark:border-danger-400/60 dark:text-danger-300/85' => Variant::Outline->is($variant) && Color::Danger->is($color),
+    'hover:bg-danger-300/50 hover:border-danger-400/50 hover:text-danger-900 dark:hover:bg-danger-400/20 dark:hover:text-danger-100' => $attributes->has('href') && Variant::Outline->is($variant) && Color::Danger->is($color),
+
+    {{-- Sizes --}}
+    'p-0.5 text-[0.675rem]' => Size::SM->is($size),
+    'px-1.5 py-1 text-xs' => Size::BASE->is($size),
+    'px-2 py-1 text-sm' => Size::MD->is($size),
+    'px-2 py-1 text-base' => Size::LG->is($size),
+
+    {{-- Border Radius --}}
+    'rounded-none' => Size::NONE->is($radius),
+    'rounded-sm' => Size::SM->is($radius),
+    'rounded' => Size::BASE->is($radius),
+    'rounded-md' => Size::MD->is($radius),
+    'rounded-lg' => Size::LG->is($radius),
+    'rounded-xl' => Size::XL->is($radius),
+    'rounded-full' => Size::FULL->is($radius),
+
+
+    ...config('ui.focusClasses'),
+    'focus-visible:ring-primary-400/80 focus-visible:dark:ring-primary-300/80',
+]) }}
+>
+    @if ($icon)
+        <x-dynamic-component
+            :component="'ui::icon.' . $icon"
+            :variant="$iconVariant"
+            class="opacity-80 mr-2"
+        />
+    @endif
+
+    {{ $slot }}
+
+    @if ($iconAfter)
+        <x-dynamic-component
+            :component="'ui::icon.' . $iconAfter"
+            :variant="$iconVariant"
+            class="opacity-80 ml-2"
+        />
+    @endif
+</{{ $attributes->has('href') ? 'a' : 'span' }}>
---
+++ tests/Browser/BadgeTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Bearly\Ui\Tests\Browser;
+
+use Bearly\Ui\Tests\BrowserTestCase;
+
+class BadgeTest extends BrowserTestCase
+{
+    public function test_can_be_rendered()
+    {
+        $this->blade('<ui:badge dusk="badge">Hello!</ui:badge>')
+            ->assertVisible('@badge')
+            ->assertSee('Hello!');
+    }
+
+    public function test_colors()
+    {
+        $this->blade(<<<'HTML'
+            <ui:badge color="primary" dusk="primary-badge">Primary</ui:badge>
+            <ui:badge color="secondary" dusk="secondary-badge">Secondary</ui:badge>
+            <ui:badge color="success" dusk="success-badge">Success</ui:badge>
+            <ui:badge color="warning" dusk="warning-badge">Warning</ui:badge>
+            <ui:badge color="danger" dusk="danger-badge">Danger</ui:badge>
+        HTML)
+            ->assertHasClasses('@primary-badge', ['bg-primary-200/60', 'text-primary-900'])
+            ->assertHasClasses('@secondary-badge', ['bg-secondary-300/40', 'text-secondary-800'])
+            ->assertHasClasses('@success-badge', ['bg-success-200/60', 'text-success-900'])
+            ->assertHasClasses('@warning-badge', ['bg-warning-200/60', 'text-warning-900'])
+            ->assertHasClasses('@danger-badge', ['bg-danger-200/60', 'text-danger-900']);
+    }
+
+    public function test_sizes()
+    {
+        $this->blade(<<<'HTML'
+            <ui:badge size="sm" dusk="sm-badge">sm</ui:badge>
+            <ui:badge size="base" dusk="base-badge">base</ui:badge>
+            <ui:badge size="md" dusk="md-badge">md</ui:badge>
+            <ui:badge size="lg" dusk="lg-badge">lg</ui:badge>
+        HTML)
+            ->assertHasClasses('@sm-badge', ['p-0.5', 'text-[0.675rem]'])
+            ->assertHasClasses('@base-badge', ['px-1.5', 'py-1', 'text-xs'])
+            ->assertHasClasses('@md-badge', ['px-2', 'py-1', 'text-sm'])
+            ->assertHasClasses('@lg-badge', ['px-2', 'py-1', 'text-base']);
+    }
+
+    public function test_variants()
+    {
+        $this->blade(<<<'HTML'
+            <ui:badge variant="solid" color="primary" dusk="solid-badge">Solid</ui:badge>
+            <ui:badge variant="outline" color="primary" dusk="outline-badge">Outline</ui:badge>
+        HTML)
+            ->assertHasClass('@solid-badge', 'bg-primary-200/60')
+            ->assertHasClass('@solid-badge', 'text-primary-900')
+            ->assertHasClass('@outline-badge', 'bg-transparent')
+            ->assertHasClass('@outline-badge', 'border-primary-600/70')
+            ->assertHasClass('@outline-badge', 'text-primary-800');
+    }
+
+    public function test_border_radius()
+    {
+        $this->blade(<<<'HTML'
+            <ui:badge icon-after="arrow-right" radius="none" dusk="none-radius-badge">None</ui:badge>
+            <ui:badge radius="sm" dusk="sm-radius-badge">Small</ui:badge>
+            <ui:badge radius="base" dusk="base-radius-badge">Base</ui:badge>
+            <ui:badge radius="md" dusk="md-radius-badge">Medium</ui:badge>
+            <ui:badge radius="lg" dusk="lg-radius-badge">Large</ui:badge>
+            <ui:badge radius="xl" dusk="xl-radius-badge">Extra Large</ui:badge>
+            <ui:badge radius="full" dusk="full-radius-badge">Full</ui:badge>
+        HTML)
+            ->assertHasClass('@none-radius-badge', 'rounded-none')
+            ->assertHasClass('@sm-radius-badge', 'rounded-sm')
+            ->assertHasClass('@base-radius-badge', 'rounded')
+            ->assertHasClass('@md-radius-badge', 'rounded-md')
+            ->assertHasClass('@lg-radius-badge', 'rounded-lg')
+            ->assertHasClass('@xl-radius-badge', 'rounded-xl')
+            ->assertHasClass('@full-radius-badge', 'rounded-full');
+    }
+
+    public function test_with_icon()
+    {
+        $this->blade('<ui:badge icon="arrow-left" dusk="badge"><span>Badge</span></ui:badge>')
+            ->assertMissing('@badge svg:last-child')
+            ->assertPresent('@badge svg:first-child')
+            ->assertHasClass('@badge svg:first-child', 'mr-2');
+    }
+
+    public function test_with_icon_after()
+    {
+        $this->blade('<ui:badge icon-after="arrow-right" dusk="badge"><span>Badge</span></ui:badge>')
+            ->assertMissing('@badge svg:first-child')
+            ->assertPresent('@badge svg:last-child')
+            ->assertHasClass('@badge svg:last-child', 'ml-2');
+    }
+
+    public function test_custom_icon_variant()
+    {
+        $this->blade('<ui:badge icon="arrow-left" icon-variant="micro" dusk="badge"><span>Badge</span></ui:badge>')
+            ->assertHasClass('@badge svg', 'size-4');
+    }
+
+    public function test_href_prop()
+    {
+        $this->blade('<ui:badge href="https://example.com/" dusk="link-badge">Link Badge</ui:badge>')
+            ->assertVisible('@link-badge')
+            ->assertAttribute('@link-badge', 'href', 'https://example.com/')
+            ->assertSee('Link Badge');
+    }
+}
