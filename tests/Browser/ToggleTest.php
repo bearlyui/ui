39c9ed92f72cb73cb7b8d74285863da523a294bf
Austen Cameron <austen.cameron@gmail.com>
2025-01-02 21:08:57 -0700

    wip

--- tests/Browser/ToggleTest.php
+++ tests/Browser/ToggleTest.php
@@ -241,13 +241,13 @@ class ExampleLivewireToggleArray extends Component
     {
         return <<<'HTML'
             <div>
-                <ui:group class="flex flex-col gap-2 p-5">
+                <div class="flex flex-col gap-2 p-5">
                     @foreach (['one', 'two', 'three'] as $item)
                         <div class="flex items-center gap-2" wire:key="{{ $item }}">
                             <ui:toggle dusk="toggle-{{ $item }}" value="{{ $item }}" wire:model.live="selection" /> {{ $item }}
                         </div>
                     @endforeach
-                </ui:group>
+                </div>
 
                 <ui:button dusk="set-1-3" wire:click="$set('selection', ['one', 'three'])">Set 1,3</ui:button>
                 <ui:button dusk="set-2" wire:click="$set('selection', ['two'])">Set 2</ui:button>
