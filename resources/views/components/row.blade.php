29b22c773c7f8f58117eb04f49c3de819b10bb4b
Austen Cameron <austen.cameron@gmail.com>
2025-01-10 18:00:41 -0500

    wip

--- resources/views/components/row.blade.php
+++ resources/views/components/row.blade.php
@@ -1,14 +1,9 @@
 @use('Bearly\Ui\Size')
 
-@aware([
-    'hover' => false
-])
-
 <tr {{ $attributes->class([
     'group/row',
     'last:border-b-0',
     'border-b border-gray-300/70 dark:border-gray-700/70',
-    'hover:bg-gray-50 dark:hover:bg-gray-700' => $hover,
 ]) }}>
     {{ $slot }}
 </tr>
